c116895fd0c03340af7b6cd7d14f6330a2b0a182
AhmAbdallah <ahmabdallah5@gmail.com>
2019-07-11 18:12:15 +0300

    Finished the HomePAge item

--- SuperMarkets/HomePage.swift
+++ SuperMarkets/HomePage.swift
@@ -8,12 +8,50 @@
 
 import SwiftUI
 
+
+struct Market: Identifiable {
+    var id: Int
+    
+    let name, imageName: String
+}
+
 struct HomePage : View {
+    
+    let markets: [Market] = [.init(id: 0, name: "Çağdaş", imageName: "Cagdas"),
+                             .init(id: 1, name: "Migros", imageName: "Migros"),
+                             .init(id: 2, name: "BIM", imageName: "BIM"),
+                             .init(id: 3, name: "A-101", imageName: "A-101"),
+                             .init(id: 4, name: "Carrefour", imageName: "Carrefour"),
+                             .init(id: 5, name: "Şok", imageName: "Sok")]
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView {
+            List {
+                ForEach(markets.identified(by: \.id)) { market in
+                    
+                    MarketRow(market: market)
+                }
+            }
+        }.navigationBarTitle(Text("Markets"))
+        
     }
 }
 
+struct MarketRow: View {
+    let market: Market
+    var body: some View {
+        HStack{
+            Image(market.imageName)
+                .resizable()
+                .padding(.all, 10.0)
+                .clipShape(Circle())
+                .frame(width: 80, height: 80)
+            Text(market.name)
+                .multilineTextAlignment(.leading)
+            
+        }
+    }
+}
 #if DEBUG
 struct HomePage_Previews : PreviewProvider {
     static var previews: some View {
